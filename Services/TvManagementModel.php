5f906e52da7db6811e9c710ace2741b04e842f89
Can Berkol <can.berkol@biberltd.com>
2016-04-01 17:31:50 +0300

    bug fix

--- Services/TvManagementModel.php
+++ Services/TvManagementModel.php
@@ -1971,10 +1971,10 @@ class TvManagementModel extends CoreModel{
 				case 'actual_time':
 				case 'end_time':
 				case 'duration':
-					$tvpsSortOrder[] = array($key => $value);
+					$tvpsSortOrder = array($key => $value);
 					break;
 				default:
-					$tvpSortOrder[] = array($key => $value);
+					$tvpSortOrder = array($key => $value);
 					break;
 			}
 		}
@@ -2045,10 +2045,10 @@ class TvManagementModel extends CoreModel{
 				case 'actual_time':
 				case 'end_time':
 				case 'duration':
-					$tvpsSortOrder[] = array($key => $value);
+					$tvpsSortOrder = array($key => $value);
 					break;
 				default:
-					$tvpSortOrder[] = array($key => $value);
+					$tvpSortOrder = array($key => $value);
 					break;
 			}
 		}
@@ -2261,10 +2261,10 @@ class TvManagementModel extends CoreModel{
 				case 'actual_time':
 				case 'end_time':
 				case 'duration':
-					$tvpsSortOrder[] = array($key => $value);
+					$tvpsSortOrder = array($key => $value);
 					break;
 				default:
-					$tvpSortOrder[] = array($key => $value);
+					$tvpSortOrder = array($key => $value);
 					break;
 			}
 		}
@@ -2499,10 +2499,10 @@ class TvManagementModel extends CoreModel{
 				case 'actual_time':
 				case 'end_time':
 				case 'duration':
-					$tvpsSortOrder[] = array($key => $value);
+					$tvpsSortOrder = array($key => $value);
 					break;
 				default:
-					$tvpSortOrder[] = array($key => $value);
+					$tvpSortOrder = array($key => $value);
 					break;
 			}
 		}
@@ -2784,10 +2784,10 @@ class TvManagementModel extends CoreModel{
 				case 'actual_time':
 				case 'end_time':
 				case 'duration':
-					$tvpsSortOrder[] = array($key => $value);
+					$tvpsSortOrder = array($key => $value);
 					break;
 				default:
-					$tvpSortOrder[] = array($key => $value);
+					$tvpSortOrder = array($key => $value);
 					break;
 			}
 		}
